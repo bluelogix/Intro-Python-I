ff89a8862fd03bcf7f45808ca9857ea1ce772015
bluelogix <bluelogix010@gmail.com>
2019-04-29 17:15:59 -0400

    modules.py

--- src/modules.py
+++ src/modules.py
@@ -10,22 +10,31 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+print("Command Line Arguments:")
+for z in sys.argv:
+    print(z)
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print("Platform:")
+for z in sys.platform:
+    print(z)
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print("Version:")
+print(sys.version_info)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
-
+print("PID current process:")
+print(os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print("CWD:")
+print(os.getcwd())
 # Print out your machine's login name
 # YOUR CODE HERE
+print("Login Name:")
+print(os.getlogin())
